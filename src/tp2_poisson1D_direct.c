29fc7e0719e9f4adc36f1b85129068d2530b1e09
byjtew93 <benjamin.lozes@protonmail.com>
2021-12-09 16:34:53 +0100

    Reformat

--- src/tp2_poisson1D_direct.c
+++ src/tp2_poisson1D_direct.c
@@ -4,90 +4,90 @@
 /* to solve the Poisson 1D problem        */
 /******************************************/
 #include "lib_poisson1D.h"
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 /* ** argc: Number of arguments */
 /* ** argv: Values of arguments */
 {
-  int ierr;
-  int jj;
-  int nbpoints, la;
-  int ku, kl, kv, lab;
-  int *ipiv;
-  int info;
-  int NRHS;
-  double T0, T1;
-  double *RHS, *EX_SOL, *X;
-  double *AB;
+    int ierr;
+    int jj;
+    int nbpoints, la;
+    int ku, kl, kv, lab;
+    int *ipiv;
+    int info;
+    int NRHS;
+    double T0, T1;
+    double *RHS, *EX_SOL, *X;
+    double *AB;
 
-  double temp, relres;
+    double temp, relres;
 
-  NRHS=1;
-  nbpoints=12;
-  la=nbpoints-2;
-  T0=-5.0;
-  T1=5.0;
+    NRHS = 1;
+    nbpoints = 12;
+    la = nbpoints - 2;
+    T0 = -5.0;
+    T1 = 5.0;
 
-  printf("--------- Poisson 1D ---------\n\n");
-  RHS=(double *) malloc(sizeof(double)*la);
-  EX_SOL=(double *) malloc(sizeof(double)*la);
-  X=(double *) malloc(sizeof(double)*la);
+    printf("--------- Poisson 1D ---------\n\n");
+    RHS = (double *)malloc(sizeof(double) * la);
+    EX_SOL = (double *)malloc(sizeof(double) * la);
+    X = (double *)malloc(sizeof(double) * la);
 
-  set_grid_points_1D(X, &la);
-  set_dense_RHS_DBC_1D(RHS,&la,&T0,&T1);
-  set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
-  
-  write_vec(RHS, &la, "RHS.dat");
-  write_vec(EX_SOL, &la, "EX_SOL.dat");
-  write_vec(X, &la, "X_grid.dat");
+    set_grid_points_1D(X, &la);
+    set_dense_RHS_DBC_1D(RHS, &la, &T0, &T1);
+    set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
 
-  kv=1;
-  ku=1;
-  kl=1;
-  lab=kv+kl+ku+1;
+    write_vec(RHS, &la, "RHS.dat");
+    write_vec(EX_SOL, &la, "EX_SOL.dat");
+    write_vec(X, &la, "X_grid.dat");
 
-  AB = (double *) malloc(sizeof(double)*lab*la);
+    kv = 1;
+    ku = 1;
+    kl = 1;
+    lab = kv + kl + ku + 1;
 
-  info=0;
+    AB = (double *)malloc(sizeof(double) * lab * la);
 
-  /* working array for pivot used by LU Factorization */
-  ipiv = (int *) calloc(la, sizeof(int));
+    info = 0;
 
-  int row = 1; //
+    /* working array for pivot used by LU Factorization */
+    ipiv = (int *)calloc(la, sizeof(int));
 
-  if (row == 1){ // LAPACK_ROW_MAJOR
-    set_GB_operator_rowMajor_poisson1D(AB, &lab, &la, &kv);
-    //write_GB_operator_rowMajor_poisson1D(AB, &lab, &la, "AB_row.dat");
-    
-    info = LAPACKE_dgbsv(LAPACK_ROW_MAJOR,la, kl, ku, NRHS, AB, la, ipiv, RHS, NRHS);
+    int row = 1;  //
 
-  } 
-  else { // LAPACK_COL_MAJOR
-    set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
-    //write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "AB_col.dat");
+    if (row == 1) {  // LAPACK_ROW_MAJOR
+        set_GB_operator_rowMajor_poisson1D(AB, &lab, &la, &kv);
+        // write_GB_operator_rowMajor_poisson1D(AB, &lab, &la, "AB_row.dat");
 
-    info = LAPACKE_dgbsv(LAPACK_COL_MAJOR,la, kl, ku, NRHS, AB, lab, ipiv, RHS, la);
-  }    
+        info = LAPACKE_dgbsv(LAPACK_ROW_MAJOR, la, kl, ku, NRHS, AB, la, ipiv,
+                             RHS, NRHS);
 
-  
-  printf("\n INFO DGBSV = %d\n",info);
+    } else {  // LAPACK_COL_MAJOR
+        set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
+        // write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "AB_col.dat");
 
-  write_xy(RHS, X, &la, "SOL.dat");
+        info = LAPACKE_dgbsv(LAPACK_COL_MAJOR, la, kl, ku, NRHS, AB, lab, ipiv,
+                             RHS, la);
+    }
 
-  /* Relative residual */
-  temp = cblas_ddot(la, RHS, 1, RHS,1);
-  temp = sqrt(temp);
-  cblas_daxpy(la, -1.0, RHS, 1, EX_SOL, 1);
-  relres = cblas_ddot(la, EX_SOL, 1, EX_SOL,1);
-  relres = sqrt(relres);
-  relres = relres / temp;
-  
-  printf("\nThe relative residual error is relres = %e\n",relres);
+    printf("\n INFO DGBSV = %d\n", info);
 
-  free(RHS);
-  free(EX_SOL);
-  free(X);
-  free(AB);
-  free(ipiv);
+    write_xy(RHS, X, &la, "SOL.dat");
 
-  printf("\n\n--------- End -----------\n");
+    /* Relative residual */
+    temp = cblas_ddot(la, RHS, 1, RHS, 1);
+    temp = sqrt(temp);
+    cblas_daxpy(la, -1.0, RHS, 1, EX_SOL, 1);
+    relres = cblas_ddot(la, EX_SOL, 1, EX_SOL, 1);
+    relres = sqrt(relres);
+    relres = relres / temp;
+
+    printf("\nThe relative residual error is relres = %e\n", relres);
+
+    free(RHS);
+    free(EX_SOL);
+    free(X);
+    free(AB);
+    free(ipiv);
+
+    printf("\n\n--------- End -----------\n");
 }
